91588040c0cb2a11de91bd8a8c26acc6cceca943
PRIYANSHU GUPTA <118977399+psgupta712@users.noreply.github.com>
2026-02-27 03:59:09 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,12 +19,10 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: [
-    "https://gupta-hotels-mdiqd1hu1-psgupta712s-projects.vercel.app",
-    "http://localhost:3000",
-  ],
+  origin: ["http://localhost:3000", "http://localhost:5173"],
   credentials: true,
 }));
+
 
 app.use(express.json());
 app.use(cookieParser());
