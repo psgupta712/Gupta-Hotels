a5b434fe7eae8f7032174caa84ec8af3f13dca4f
PRIYANSHU GUPTA <118977399+psgupta712@users.noreply.github.com>
2026-02-27 03:57:30 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://gupta-hotels-backend.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
   withCredentials: true,
 });
 
