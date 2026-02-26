2bf5a3e0af49a9d67da95a7afc8bbbd20b417a22
PRIYANSHU GUPTA <118977399+psgupta712@users.noreply.github.com>
2026-02-27 03:51:22 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,8 @@
-// src/api/axios.js
-
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api", // ← matches your backend PORT 8000
-  withCredentials: true,                // ← needed to send JWT cookies
+  baseURL: "https://gupta-hotels-backend.onrender.com/api",
+  withCredentials: true,
 });
 
 export default axiosInstance;
